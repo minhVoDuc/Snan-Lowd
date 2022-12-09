a9aa69bbf763fc8a2ef9b5f6f32575f19d361b28
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-12-10 06:35:00 +0700

    update playing sound

--- js/function.js
+++ js/function.js
@@ -1,6 +1,18 @@
-function autoplay(){
-    var r =confirm("Would You Like To AutoPlay Music?");
-    if (r == true) {
-        document.getElementById("audio").play();
-    }
-}
+const button = document.querySelector("#button");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-off');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-off');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
+});
